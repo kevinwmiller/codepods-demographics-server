88906f872f48b60e1400be0b933c5f44cbda0208
Kevin Miller <KevM1227@gmail.com>
2018-03-04 17:43:56 -0500

    Added axios as a dependency to the client. Added body-parser and cookie-parser middleware to the server

--- index.js
+++ index.js
@@ -1,7 +1,17 @@
 const express = require('express');
+const cookieParser = require('cookie-parser')
+const bodyParser = require('body-parser')
 
 const app = express();
 const port = process.env.PORT || 5000;
+
+app.use(cookieParser())
+
+// support parsing of application/json type post data
+app.use(bodyParser.json());
+
+//support parsing of application/x-www-form-urlencoded post data
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/api/test', (req, res) => {
   res.send({ express: 'From the Express Backend' });
