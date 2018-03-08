bbb7a4b85616b618f774c7b3889009f22089aaa5
Kevin Miller <KevM1227@gmail.com>
2018-03-08 15:31:23 -0500

    Added config file for ports (And more in the future). Fixed issue with local access control when client and server are running on the same machine

---
+++ config.js
@@ -0,0 +1,11 @@
+var config = {
+
+	development: {
+		port: 5000,
+	},
+	production: {
+		port: 5000
+	},
+};
+
+module.exports = config[process.env.NODE_ENV] || config.development;
--- index.js
+++ index.js
@@ -5,9 +5,11 @@
 const express = require('express');
 const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
+const cors = require('cors');
+const config = require('./config');
 
 const app = express();
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || config.port;
 
 app.use(cookieParser());
 
@@ -16,6 +18,10 @@ app.use(bodyParser.json());
 
 // support parsing of application/x-www-form-urlencoded post data
 app.use(bodyParser.urlencoded({ extended: true }));
+
+// To resolve an issue when server and client are running on the same machine
+app.use(cors());
+app.options('*', cors()) // include before other routes
 
 // Include routes from controllers. Don't define routes in this main index.js file
 app.use(require('./src/controllers'));
