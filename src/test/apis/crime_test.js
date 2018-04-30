36202b6b69d45a32c977fada0492360aef5acb42
Kevin Miller <KevM1227@gmail.com>
2018-04-29 20:55:35 -0400

    Updated crime test case. Added --exit flag to mocha to force it to exit after test completion (mocha 3 behavior)

---
+++ src/test/apis/crime_test.js
@@ -0,0 +1,37 @@
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+const server = require('../../../index.js');
+const should = chai.should();
+const expect = chai.expect;
+
+chai.use(chaiHttp);
+
+describe('Crime Endpoint', () => {
+    it('should list crimes in a 1 month range', async () => {
+        chai.request(server)
+            .get('/crime')
+            .query({
+                border: {
+                    topRight: {
+                        latitude: 39.299236,
+                        longitude:-76.609383,
+                    },
+                    bottomLeft: {
+                        latitude: 39.099236,
+                        longitude:-76.409383,
+                    }
+                },
+                startDate: '2017-05-01',
+                endDate: '2017-06-01',
+            })
+            .end((err, res) => {
+                incidents.should.be.an('array');
+                expect(incidents[0]).to.have.all.keys('agency', 'agency_type', 'crimes');
+                expect(incidents[0].crimes).to.be.an('array');
+                expect(incidents[0].crimes[0]).to.have.all.keys('id', 'caseNumber', 'categorization', 'city',
+                    'incidentAddress', 'incidentDescription', 'location', 'primaryType', 'timestamp');
+            });
+    
+
+    });
+});
