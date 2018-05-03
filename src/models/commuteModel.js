a084e63a220071e25c9643c565be4e9a54af8cb4
dlaffoo1 <36972908+dlaffoo1@users.noreply.github.com>
2018-05-03 16:11:18 -0400

    finised getting Commute Model to work

--- src/models/commuteModel.js
+++ src/models/commuteModel.js
@@ -11,61 +11,14 @@
 // Axios would be used if the model made an API call
 // const axios = require('axios');
 
-/** A 'database' that is accessed by the model
- Format is id: object
- The model would normally make a call to a real database or to an external API
- IDs in an external API may not be reliable, and we will likely not be able to create or modify data
-*/
-commuteDatabase2 = {
-    '21001': {
-      zipCode: "21001",
-      commuteTime: "100",
-    },
-    '21002': {
-        zipCode: "21002",
-        commuteTime: "200",
-      },
-      '21003': {
-        zipCode: "21003",
-        commuteTime: "300",
-      },
-      '21004': {
-        zipCode: "21004",
-        commuteTime: "400",
-      },
-      '21005': {
-        zipCode: "21005",
-        commuteTime: "500",
-      },
-      '21006': {
-        zipCode: "21006",
-        commuteTime: "600",
-      }, 
-  }
 
-
-
-
-  commuteDatabase = {
-    "8600000US00601": { "zipCode":601, "census_display_label":"ZCTA5 00601", "commuteTime":28.7, "margin_of_error":3.1},
-    "8600000US00602": { "zipCode":602, "census_display_label":"ZCTA5 00602", "commuteTime":20.9, "margin_of_error":1.4},
-    "8600000US00603": { "zipCode":603, "census_display_label":"ZCTA5 00603", "commuteTime":20.3, "margin_of_error":1.4},
-    "8600000US00606": { "zipCode":606, "census_display_label":"ZCTA5 00606", "commuteTime":34.9, "margin_of_error":11.5},
-    "8600000US00610": { "zipCode":610, "census_display_label":"ZCTA5 00610", "commuteTime":26.4, "margin_of_error":1.5},
-    "8600000US00612": { "zipCode":612, "census_display_label":"ZCTA5 00612", "commuteTime":25.4, "margin_of_error":1.4},
-    "8600000US00616": { "zipCode":616, "census_display_label":"ZCTA5 00616", "commuteTime":24.8, "margin_of_error":2.4},
-    "8600000US00617": { "zipCode":617, "census_display_label":"ZCTA5 00617", "commuteTime":24.7, "margin_of_error":2},
-    "8600000US00622": { "zipCode":622, "census_display_label":"ZCTA5 00622", "commuteTime":24.4, "margin_of_error":4.3}
-  }
-
- 
-
-  //Debug messages to console
+//Debug messages to console
 var turnOnDebug=1;
 function consoleDebug(msg)
 {
     if (turnOnDebug==1) console.log('>> '+msg);
-}
+};
+
 
 //CacheCommuteData Global Vars and Function
 var commuteDataLoaded=0;
@@ -84,20 +37,25 @@ function cacheCommuteData() {
             commuteDictionary[commuteDataJSON[i]["zip_code"]] = commuteDataJSON[i]["commute_time_mins_est"];
         };
     }
-    for(var zipCode in commuteDictionary) {
-            consoleDebug(commuteJSON(zipCode));
-    }
+
 };
 
 //return Json strign version of commute Object
 function commuteJSON(zipCode)
 {            
-    var commTime = commuteDictionary[zipCode];
-    return ':***{"_id":'+zipCode+',"zipCode":'+ zipCode +',"commuteTime":'+commTime+'}';
+    if (zipCode in commuteDictionary)
+        return { "_id" : zipCode, "zipCode":  zipCode,  "commuteTime" : commuteDictionary[zipCode] };
+
+    throw new Error(`Could not find object with zipCode ${zipCode}`);
 };
 
+function commuteJSONExists(zipCode)
+{            
+    if (zipCode in commuteDictionary)
+        return true;
 
-  
+    return false;
+};
 
 
 /** Note: The parameters given here are only an commute and will change depending on the functionality of the model
@@ -129,21 +87,27 @@ exports.getAll = async (query) => {
      However, since we only have an object here, we can manually filter our data
     */
 
-    commuteData = Object.values(commuteDatabase);
+   cacheCommuteData();
 
-    // Filter our array to only those object that match every constraint passed in the query
-    return commuteData.filter(commute => {
-        // Not sure of the one-liner method to do this yet
-        let matchesConstraints = true;
-        Object.keys(query).every((queryKey) => {
-            if (!(queryKey in commute) || commute[queryKey] !== query[queryKey]) {
-                matchesConstraints = false;
-                return;
-            }
-        });
-        return matchesConstraints;
+    //since i was not sure of the performace of searching a large JSON object
+    //I opted to build a dictionary tying the commute time (value) to each zip (key)
+    //
+    //so for now we will only support lookup by 1 zipCode
+    // if needed we can add additioan search patterns
+
+    var result=[];
+
+    Object.keys(query).every((queryKey) => {
+       if (queryKey=='zipCode') { 
+           var zipCode = query[queryKey];  
+           if (commuteJSONExists(zipCode))
+               result.push(commuteJSON(zipCode));
+        }
     });
+
+    return result;
 };
+
 
 /**
     The model would make an api request using axios or a database request if no api is available
@@ -154,16 +118,11 @@ exports.get = async (id) => {
     console.log('CommuteModel get');
 
     cacheCommuteData();
+    return commuteJSON(id);
 
-    console.log('CommuteModel get');
-    
-    if (id in commuteDatabase) {
-        return commuteDatabase[id];
-    }
-    throw new Error(`Could not find object with ID ${id}`);
+    throw new Error(`Could not find object with zipCode ${zipCode}`);
+
 };
-
-
 
 
 /**
