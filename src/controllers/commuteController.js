893d943f2b108b4979adf8f3480d985399c0360a
dlaffoo1 <36972908+dlaffoo1@users.noreply.github.com>
2018-05-04 19:08:59 -0400

    finished v1.0 of Commute Model and Controller

--- src/controllers/commuteController.js
+++ src/controllers/commuteController.js
@@ -5,33 +5,25 @@
         Uses CommuteModel
 */
 const express = require('express');
+
 const router = express.Router();
 const commuteModel = require('../models/commuteModel');
 
 /**
-    Create
-    @returns {object} @see {@link CommuteModel}
-*/
-router.post('/', async (req, res) => {
-    try {
-        res.json({ response: await commuteModel.create(req.body.zipCode, req.body.commuteTime) });
-    } catch (err) {
-        res.json({ error: err.message });
-    }
-});
-
-/**
-    Get all
+    Get commute data for zipcodes
     @returns {object} @see {@link CommuteModel}
 */
 router.get('/', async (req, res) => {
     // req.query will contain query string parameters
-    // i.e. /commute?zipCode=21009 will populate req.query.zipCode with '21009'
-    // req.params.zipCode will contains '21009' since it is a part of our route parameters
+    //      i.e. /commute?zipCode=21009 will populate req.query.zipCode with '21009'
+    //      req.params.zipCode will contains '21009' since it is a part of our route parameters
+    // you can also lookup multiple zipcodes
+    //      i.e. /commute?zipCode=21009&zipCode=21010&zipCode=21011
     try {
-        res.json({ response: await commuteModel.getAll(req.query) });
+        res.json({ response: await commuteModel.getAll(req.query.zipCode) });
     } catch (err) {
-        res.json({ error: err.message });
+        console.log(`${err.message}`);
+        res.sendStatus(422);
     }
 });
 
@@ -39,43 +31,19 @@ router.get('/', async (req, res) => {
     Get specific
     @returns {object} @see {@link CommuteModel}
 */
-router.get('/:id', async (req, res) => {
+router.get('/:zipCode', async (req, res) => {
     // req.params will contain the given id
     // req.query will contain query string parameters
     // i.e. /commute/21009 will populate req.params with '21009'
     // req.params.id will contain '21009'
     try {
-        res.json({ response: await commuteModel.get(req.params.id) });
+        res.json({ response: await commuteModel.get(req.params.zipCode) });
     } catch (err) {
-        res.json({ error: err.message });
-    }
-});
-
-/**
-    Uupdate
-    @returns {object} @see {@link CommuteModel}
-*/
-router.patch('/:id', async (req, res) => {
-    // req.body will contains key/value pairs contained in the page body.
-    // Usually only needed when a post request is received
-    try {
-        res.json({ response: await commuteModel.update(req.body.zipCode, req.body.commuteTime) });
-    } catch (err) {
-        res.json({ error: err.message });
-    }
-});
-
-/**
-    Delete
-    @returns {object} @see {@link CommuteModel}
-*/
-router.delete('/:id', async (req, res) => {
-    try {
-        res.json({ response: await commuteModel.delete(req.body.zipCode, req.body.commuteTime) });
-    } catch (err) {
-        res.json({ error: err.message });
+        console.log(`${err.message}`);
+        res.sendStatus(422);
     }
 });
 
 
 module.exports = router;
+
--- src/models/commuteModel.js
+++ src/models/commuteModel.js
@@ -1,10 +1,10 @@
 /**
     @exports CommuteModel
-    @file Models handle calls to the database or to fetch data from APIs
+    @file Manages commute data obtained from https://data.world/scxt/commute-times-by-zipcode
 
-    @typedef {Object} CommuteJsonObject
+    @typedef {Object} CommuteDetails
     @property {string} zipCode the zip code of interest
-    @property {string} commuteTome commute time (in minutes) for the zip code
+    @property {string} commuteTime commute time (in minutes) for the zip code
 
  */
 
@@ -15,21 +15,10 @@
  */
 var commuteData = require ('../modeldata/commuteData.json');
 
-
-/** 
-    Debug messages to console 
-*/
-var turnOnDebug=1;  // ***** set this to 1 to show consoleDebug msgs (0 or anything otherthan 1 to turn off)
-function consoleDebug(msg)
-{
-    if (turnOnDebug==1) console.log('>> '+msg);
-};
-
-
 /**
  *  return Json strign version of commute Object
  */
-function commuteJSON(zipCode)
+function CommuteDetailsbyZipCode(zipCode)
 { 
     var lookupZip = Number(zipCode).toString(); //zip code in db is non-zero padded number
 
@@ -41,20 +30,19 @@ function commuteJSON(zipCode)
     var paddedZip = commuteItem.zip_code.padStart(5, "0")   
     var commuteTime = commuteItem.commute_time_mins_est;
 
-    return { "_id" : paddedZip, "zipCode":  paddedZip,  "commuteTime" : commuteTime };
+    return { "zipCode":  paddedZip,  "commuteTime" : commuteTime };
 };
 
 /**
  *  is the Zip Code in commuteData?
  */
-function commuteJSONExists(zipCode)
-{            
-    if (indexByZipCode(zipCode) > 0 )
+function existsByZipCode(zipCode)
+{       
+    if (indexByZipCode(zipCode) >= 0 )
         return true;
     
     return false;
 };
-
 
 /**
  *  is the Zip Code in commuteData?
@@ -64,86 +52,41 @@ function indexByZipCode(zipCode)
     return commuteData.findIndex(x  => x.zip_code === zipCode);
 };
 
-
-
-/** Note: The parameters given here are only an commute and will change depending on the functionality of the model
-    Thrown exception messages may also vary between models as needed
-*/
 /**
-    The model would make an api request using axios or a database request if no api is available
-    The create method should create a data store record with the given parameters
-    If creating a new record is not an available option, e.g. an external API request,
-    the create method should throw an exception
-    @returns {CommuteJsonObject} The new CommuteModel data
-    @throws Will throw an error if the method is used
-*/
-exports.create = async () => {
-    console.log('CommuteModel create');
-    throw new Error('CommuteModel does not support "create"');
-};
-
-
-/**
-    The model would make an api request using axios or a database request if no api is available
-    The getAll method returns data that matches the given constraints
-    @returns {CommuteJsonObject|Array} An array CommuteJsonObject data
-*/
-exports.getAll = async (query) => {
-    console.log('CommuteModel getAll');
-    /** Get all resouce data of the model type from the database or api
-     If we were accessing a real database or an api, we could easily pass our constraints to it to handle the filtering
-     However, since we only have an object here, we can manually filter our data
-    */
-
-    //since i was not sure of the performace of searching a large JSON object
-    //I opted to build a dictionary tying the commute time (value) to each zip (key)
-    //
-    //so for now we will only support lookup by 1 zipCode
-    // if needed we can add additioan search patterns
+ * Requests crime data for a given border area from CrimeReports.com
+ *
+ * @param      {string}  zipcode  a single zipcode or an array of zipcodes
+ * @return     {CommuteDetails[]} A list of objects containing commute details for the zipcode(s)
+ */
+exports.getAll = async (zipCode) => {
+    console.log('CommuteModel getAll zipCode = '+ zipCode);
 
     var result=[];
 
-    Object.keys(query).every((queryKey) => {
-       if (queryKey=='zipCode') { 
-           var zipCode = query[queryKey];  
-           if (commuteJSONExists(zipCode))
-               result.push(commuteJSON(zipCode));
+    if (Array.isArray(zipCode)) {
+        for (var i = 0; i < zipCode.length; i++) {
+   //             if (existsByZipCode(zipCode[i]))
+                    result.push(CommuteDetailsbyZipCode(zipCode[i]));
         }
-    });
+    } else {
+
+  //      if (existsByZipCode(zipCode))
+               result.push(CommuteDetailsbyZipCode(zipCode));
+    }
 
     return result;
 };
 
 
 /**
-    The model would make an api request using axios or a database request if no api is available
-    The get method returns data that matches the given constraints
-    @returns {CommuteJsonObject|Array} An array CommuteJsonObject data
-*/
-exports.get = async (id) => {
-    console.log('CommuteModel get');
-    return commuteJSON(id);
+ * Requests crime data for a given border area from CrimeReports.com
+ *
+ * @param      {string}  zipCode    a single zipcode 
+ * @return     {CommuteDetails}     a object containing commute details for the zipcode
+ */
+exports.get = async (zipCode) => {
+    console.log('CommuteModel get zipCode = '+ zipCode);
+    return CommuteDetailsbyZipCode(zipCode);
 };
 
 
-/**
-    The model would make an api request using axios or a database request if no api is available
-    The edit method will edit an existing data store record with the given parameters
-    @returns {CommuteJsonObject} The new CommuteModel data
-    @throws Will throw an error if the method is used.
-*/
-exports.update = async () => {
-    console.log('CommuteModel edit');
-    throw new Error('CommuteModel does not support "update"');
-};
-
-/**
-    The model would make an api request using axios or a database request if no api is available
-    The delete method will edit an existing data store record with the given parameters
-    @returns {undefined}
-    @throws Will throw an error if the method is used
-*/
-exports.delete = async () => {
-    console.log('CommuteModel delete');
-    throw new Error('CommuteModel does not support "delete"');
-};
