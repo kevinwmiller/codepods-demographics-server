dee69e715119fb498882503b2524860c9251a3a6
Unknown <dave.pan1@gmail.com>
2018-05-13 12:07:54 -0400

    Added some details to income modeling
    Added geojson data for the counties so we can come up with a coloring for income data. Mostly copied this from the commute data. Needs to be called by the frontend.

--- src/models/incomeModel.js
+++ src/models/incomeModel.js
@@ -9,7 +9,42 @@
 
 const incomeReports = require('../apis/incomeReports') ;
 
+const countyDataGeometry = require('../modeldata/Maryland_Counties_CensusGov.geojson')
 
+
+function getIncomeByCounty(countyName) => {
+   incomeReports.fetchIncomeData(countyName);
+}
+function getIncomeByMapBounds() => {
+    if (!border || !border.topRight || !border.bottomLeft
+            || !border.topRight.latitude || !border.bottomLeft.latitude
+            || !border.topRight.longitude || !border.bottomLeft.longitude) {
+                throw new Error(`Invalid parameter: border '${border}'` );
+    }
+    const smallestLat = border.bottomLeft.latitude;
+    const smallestLng = border.bottomLeft.longitude;
+    const largestLat = border.topRight.latitude;
+    const largestLng = border.topRight.longitude;
+
+    // Create an array that contains the string  
+    // values that are in the original array.  
+
+    const foundCounty1 = countyDataGeometry.filter(
+        function (value) {
+            return (
+                   Number(value.latitude) >= Number(smallestLat)
+                && Number(value.latitude) <= Number(largestLat)
+                && Number(value.longitude) >= Number(smallestLng)
+                && Number(value.longitude) <= Number(largestLng)
+            );
+        });
+    const results = [];
+    let foundCounty = foundCounty1;
+    for (let i = 0; i < foundCounty.length; ++i) {
+        results.push(getIncomeByCounty(foundCounty[i].NAME));
+    }
+    return results;
+}
 /**
     Requests income data for a given county from data.gov
     
